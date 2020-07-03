a02c32026e24027373d8830c8531ff118c8a83df
boyloe <boyloe@gmail.com>
2020-07-03 11:22:41 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+function getFirstSelector(selector) {
+    const firstSel = document.querySelector(selector)
+    return firstSel
+}    
 
+function nestedTarget() {
+    const nestedTarget = document.getElementById("nested").querySelector(".target")
+    return nestedTarget
+}
+
+function increaseRankBy(n) {
+    const rankList = document.querySelectorAll('ul.ranked-list li');
+  
+    for (var i = 0; i < rankList.length; i++) {
+      rankList[i].innerHTML = parseInt(rankList[i].innerHTML) + parseInt(n);
+    }
+  }
+  
+  function deepestChild(){
+    const dChild = document.getElementById('grand-node').querySelectorAll('div')
+    return dChild[dChild.length-1]
+  
+  }
